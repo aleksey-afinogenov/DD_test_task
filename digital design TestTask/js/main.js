27a04d80b06c7c758c2e43bc301b7a6c5eed136d
aleksey-afinogenov <134380284+aleksey-afinogenov@users.noreply.github.com>
2023-05-26 21:45:07 +0400

    Update main.js

--- digital design TestTask/js/main.js
+++ digital design TestTask/js/main.js
@@ -29,7 +29,7 @@ for (let elem of buttons) {
   })
 }
 
-//закрыть форму
+//закрваем форму
 const closeModals = document.querySelectorAll('.close-modal');
 
 for (let el of closeModals) {
@@ -52,7 +52,7 @@ sendForm.addEventListener('click', function() {
   newModal.classList.remove('hidden');
 })
 
-//themes
+//смена темы
 
 const flag = document.querySelector('.themes__option');
 
@@ -60,7 +60,7 @@ flag.addEventListener('click', function() {
   document.documentElement.classList.toggle('dark');
 })
 
-//data
+//обработка даты
 const dataAllText = document.querySelectorAll('.data');
 
 function getDayInfo(str) {
@@ -75,14 +75,13 @@ function getDayInfo(str) {
     if (num > 28 && num <31)  {return '5 неделя';}
     else{return 'неверная дата';}
   }
-
   let arr = str.split('.');
   let date = new Date(arr[2], arr[1]-1, arr[0]);
 
   let newStr = days[date.getDay()] + ', ' + getWeekNumber(date.getDate()) + ' '+ months[date.getMonth()] + ' ' + date.getFullYear()+ ' ' + 'года.';
   return newStr;
-  
 }
+
 for (let elem of dataAllText) {
 elem.textContent = getDayInfo(elem.textContent);
 }
